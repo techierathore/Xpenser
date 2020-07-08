dc04846ad4c38aff8b7a21d0ce43f2085fc39ed8
S Ravi Kumar <techierathore@gmail.com>
2020-07-08 21:29:47 +0530

    8th July Streaming Code Changes , Created the Menu and Updated the Login Page

---
+++ Source/Xpenser.Models/SvcData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xpenser.Models
+{
+	public class SvcData
+	{
+		public string ComplexData { get; set; }
+		public string OrgCode { get; set; }
+		public string LoginEmail { get; set; }
+		public string LoginPass { get; set; }
+		public string JwToken { get; set; }
+	}
+}
---
+++ Source/Xpenser.UI/Pages/LoginPage.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xpenser.Models;
+
+namespace Xpenser.UI.Pages
+{
+    public class LoginBase : ComponentBase
+    {
+        public SvcData LoginDetails { get; set; } = new SvcData();
+        public string LoginMesssage { get; set; }
+    }
+}

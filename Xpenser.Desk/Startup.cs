1baca27c31853951f3f28f83019e4d164f81f02c
S Ravi Kumar <techierathore@gmail.com>
2021-06-11 23:32:19 +0530

    Implimented Photino in Desktop Project

--- Xpenser.Desk/Startup.cs
+++ Xpenser.Desk/Startup.cs
@@ -1,62 +1,50 @@
-﻿using Blazorise;
+﻿using Blazored.LocalStorage;
+using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Photino.Blazor;
+using Xpenser.Models;
+using Xpenser.UI;
+using Xpenser.UI.Services;
 
 namespace BlazorDesk
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-.AddBlazorise(options =>
-{
-    options.ChangeTextOnKeyPress = true;
-})
-.AddBootstrapProviders()
-.AddFontAwesomeIcons();
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddBlazorise(options =>
+            {
+                options.ChangeTextOnKeyPress = true;
+            })
+        .AddBootstrapProviders()
+        .AddFontAwesomeIcons();
+            /*
+var appSettingSection = Configuration.GetSection("AppSettings");
+services.Configure<AppSettings>(appSettingSection);
+services.AddTransient<ValidateHeaderHandler>();
+services.AddBlazoredLocalStorage();
+
+services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+services.AddHttpClient<IAuthService, AuthService>();
+
+services.AddHttpClient<IManageService<Account>, ManageService<Account>>()
+        .AddHttpMessageHandler<ValidateHeaderHandler>();
+services.AddHttpClient<IManageService<Category>, ManageService<Category>>()
+        .AddHttpMessageHandler<ValidateHeaderHandler>();
+services.AddHttpClient<IManageService<ReccuringTransaction>, ManageService<ReccuringTransaction>>()
+       .AddHttpMessageHandler<ValidateHeaderHandler>();
+
+*/
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(DesktopApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
-            });
+            app.AddComponent<App>("app");
         }
     }
 }
